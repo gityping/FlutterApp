1f4cb0e18f2a2c0e1eba70f0a419a87e7ef3b6d3
ping <yangp@govlan.com>
2019-06-13 15:52:41 +0800

    图片轮播

---
+++ my_flutter_app/lib/pages/api/CustomApi.dart
@@ -0,0 +1,5 @@
+List swiper_data = [
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560402783023&di=f4cb5efbd891bdf5bfa8fe1b9395630a&imgtype=0&src=http%3A%2F%2Fcdn0.hbimg.cn%2Fstore%2Fwm%2Fpiccommon%2F1198%2F11984%2FD525A3C998BBFFB18A8DBC941A.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560402833243&di=9a7589fa240dac6a765a06b5873273ee&imgtype=0&src=http%3A%2F%2Fimg3.ph.126.net%2F1KDLXCf_5HxC0KDAxUQZSg%3D%3D%2F2866541162838628463.jpg',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1560402833243&di=9a7589fa240dac6a765a06b5873273ee&imgtype=0&src=http%3A%2F%2Fimg3.ph.126.net%2F1KDLXCf_5HxC0KDAxUQZSg%3D%3D%2F2866541162838628463.jpg'
+];
--- my_flutter_app/lib/pages/tabs/HomePage.dart
+++ my_flutter_app/lib/pages/tabs/HomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import '../api/CustomApi.dart';
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
 
@@ -9,7 +11,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-       child: Text('data'),
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 200.0,
+            child: Swiper(
+              itemBuilder: _swiperBuilder,
+              itemCount: 3,
+              pagination: SwiperPagination(
+                builder: DotSwiperPaginationBuilder(
+                  activeColor: Colors.blue
+                )
+              ),
+              // control: new SwiperControl(),
+              scrollDirection: Axis.horizontal,
+              autoplay: true,
+              onTap: (index) {
+                print("object");
+              },
+            ),
+          ),
+          Container(
+            child: Row(
+              children: <Widget>[
+                MyIcons(Icons.home, size: 32.0, color: Colors.yellow,),
+                MyIcons(Icons.search, size: 32.0, color: Colors.yellow,),
+                MyIcons(Icons.games, size: 32.0, color: Colors.yellow,),
+                MyIcons(Icons.nature, size: 32.0, color: Colors.yellow,),
+              ],
+            ),
+          )
+        ],
+      )
+    );
+  }
+}
+Widget _swiperBuilder(BuildContext context, int index) {
+  print(index);
+  return (Image.network(
+    swiper_data[index],
+    fit: BoxFit.fill,
+  ));
+}
+class  MyIcons extends StatelessWidget {
+  IconData icons;
+  double size=32.0;
+  Color color;
+  MyIcons(this.icons,{this.size, this.color});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      color: this.color,
+      child: Center(
+        child: Icon(
+          this.icons,
+          size: this.size,
+          color: Colors.white,
+        ),
+      ),
     );
   }
 }
